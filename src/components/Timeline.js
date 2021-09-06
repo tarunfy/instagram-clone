ff6f1482a8cb218b393c095441f7ec2eb0a30367
tarun2506 <tarunsharma8920@gmail.com>
2021-09-06 22:58:38 +0530

    timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
 
 function Timeline() {
+  const { photos } = usePhotos();
+
   return (
     <div className="container col-span-2">
-      <p>Timeline</p>
+      {!photos ? (
+        <>
+          {[...new Array(4)].map((_, index) => {
+            <Skeleton key={index} count={1} height={400} width={320} />;
+          })}
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((photo) => {
+          return <p key={photo.docId}>{photo.imageSrc}</p>;
+        })
+      ) : (
+        <p className="text-center text-2xl">Follow someone to see photos</p>
+      )}
     </div>
   );
 }
