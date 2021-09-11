4cce8194055548cce3f3d02f340b642ea3aa6c6e
tarun2506 <tarunsharma8920@gmail.com>
2021-09-11 17:23:33 +0530

    working on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { getUserByUsername } from "../services/firebase";
+import * as ROUTES from "../constants/routes";
+
 export default function Profile() {
-  return null;
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+  const [userExists, setUserExists] = useState(false);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const user = await getUserByUsername(username);
+      console.log("doesUserExists", user);
+      if (user.length > 0) {
+        setUser(user[0]);
+        setUserExists(true);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+    checkUserExists();
+  }, [username, history]);
+  return username ? (
+    <div className="bg-gray-background">
+      <div className="mx-auto max-w-screen-lg">{user.fullName}</div>
+    </div>
+  ) : null;
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,6 +10,18 @@ export async function doesUsernameExists(username) {
 
   return result.docs.map((user) => user.data().length > 0);
 }
+export async function getUserByUsername(username) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("username", "==", username)
+    .get();
+
+  return result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+}
 
 //getting back the user from firebase where userId is same as the given userId, or basically with the help of the userId of the authenticated user we are getting back its data from database;
 export async function getUserByUserId(userId) {
